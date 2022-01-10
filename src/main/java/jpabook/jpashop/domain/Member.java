7e3fae1cd5b0065c8cab293a4970af31caf5eb03
Juhan <juhan211@naver.com>
2022-01-10 20:59:50 +0900

    실전 예제 6 - 값 타입 매핑 - 임베디드와 equal hashcode 추가

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -11,9 +11,9 @@ public class Member extends BaseEntity{
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
 
     @OneToMany(mappedBy = "MEMBER_ID")
     private List<Member> members = new ArrayList<>();
@@ -36,27 +36,19 @@ public class Member extends BaseEntity{
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
-    public String getStreet() {
-        return street;
+    public List<Member> getMembers() {
+        return members;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
